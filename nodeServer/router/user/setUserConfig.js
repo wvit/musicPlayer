b868a26787aeda82f3697b04739bc3861f8f6b82
wvit <1999wuwei@gmail.com>
2019-02-01 21:27:47 +0800

    null

--- nodeServer/router/user/setUserConfig.js
+++ nodeServer/router/user/setUserConfig.js
@@ -4,7 +4,7 @@ const {
 const UserConfig = require('../../mongo/schema/userConfig');
 
 //接受用户注册请求
-router.post("/user/signUp", async ctx => {
+router.get("/user/setConfig", async ctx => {
   await savaUserConfig(ctx.request.query).then(data => {
     ctx.body = data;
   })
@@ -14,34 +14,17 @@ router.post("/user/signUp", async ctx => {
 function savaUserConfig(reqData) {
   return new Promise(resolve => {
     UserConfig.findOne({
-        // userId: reqData.
-    }, (err, users) => {
-        resolve(users);
+      userId: reqData.userId
+    }, (err, config) => {
+      resolve(config);
     })
-}).then(user => {
-    const password = crypto.createHash('md5').update(reqData.password + '260817').digest('hex');
+  }).then(config => {
     const resData = {
-        code: 0,
-        data: {}
-    }
-    if (!user) {
-        resData.code = 2;
-        resData.data.text = '用户不存在';
-    } else if (user.password !== password) {
-        resData.code = -1;
-        resData.data.text = '密码错误';
-    } else {
-        resData.data = {
-            userInfo: {
-                nickname: user.nickname,
-                email: user.email,
-                userId: user.userId
-            },
-            text: '登录成功，正在跳转...'
-        }
+      code: 0,
+      data: {}
     }
     return new Promise(resolve => {
-        resolve(resData)
+      resolve(resData)
     })
-})
+  })
 }
